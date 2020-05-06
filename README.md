32140a390be7dcbd4c985ec46d98bc916d61043f
Hugo Bezerra Pimentel <hugobezerrapimentel@gmail.com>
2020-05-05 21:33:36 -0300

    [INIT] First commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Flutter - Receitas culinárias
+
+Projeto de receitas culinárias, feito em Flutter e com o objetido de estudos e consultas.
+
+Atenção: o aplicativo armazena as informações em memória, em tempo de execução.
---
+++ lib/main.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:meals/data/dummy_data.dart';
+import 'package:meals/models/settings.dart';
+import 'package:meals/screens/categories_meals_screen.dart';
+import 'package:meals/screens/categories_screen.dart';
+import 'package:meals/screens/meal_detail_screen.dart';
+import 'package:meals/screens/settings_screen.dart';
+import 'package:meals/screens/tabs_screen.dart';
+import 'package:meals/utils/app_routes.dart';
+
+import 'models/meal.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<Meal> _availableMeals = DUMMY_MEALS;
+  List<Meal> _favoriteMeals = [];
+  Settings settings = Settings();
+
+  void _filterMeals(Settings settings) {
+    setState(() {
+      this.settings = settings;
+
+      _availableMeals = DUMMY_MEALS.where((meal) {
+        final filterGluten = settings.isGlutenFree && !meal.isGlutenFree;
+        final filterLactose = settings.isLactoseFree && !meal.isLactoseFree;
+        final filterVegan = settings.isVegan && !meal.isVegan;
+        final filterVegeratian = settings.isVegetarian && !meal.isVegetarian;
+
+        return !filterGluten &&
+            !filterLactose &&
+            !filterVegan &&
+            !filterVegeratian;
+      }).toList();
+    });
+  }
+
+  void _toggleFavorite(Meal meal) {
+    setState(() {
+      _favoriteMeals.contains(meal)
+          ? _favoriteMeals.remove(meal)
+          : _favoriteMeals.add(meal);
+    });
+  }
+
+  bool _isFavorite(Meal meal){
+    return _favoriteMeals.contains(meal);
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DeliMeals',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        fontFamily: 'Raleway',
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        textTheme: ThemeData.light().textTheme.copyWith(
+              title: TextStyle(fontSize: 20, fontFamily: 'RobotoCondensed'),
+            ),
+      ),
+      routes: {
+        AppRoutes.HOME: (context) => TabsScreen(_favoriteMeals),
+        AppRoutes.CATEGORIES_MEALS: (context) =>
+            CategoriesMealsScreen(_availableMeals),
+        AppRoutes.MEAL_DETAIL: (context) => MealDetailScreen(_toggleFavorite, _isFavorite),
+        AppRoutes.SETTINGS: (context) => SettingsScreen(_filterMeals, settings),
+      },
+//      onGenerateRoute: (settings) {
+//        if (settings.name == '/alguma-coisa') {
+//          return null;
+//        } else {
+//          return MaterialPageRoute(builder: (_) {
+//            return CategoriesScreen();
+//          },
+//         );
+//        }
+//      },
+//      onUnknownRoute: (settings) {
+//        return MaterialPageRoute(
+//          builder: (_) {
+//            return CategoriesScreen();
+//          },
+//        );
+//      },
+    );
+  }
+}
